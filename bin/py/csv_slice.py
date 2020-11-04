73b7e7c66b8478a8a4647b8ea975239d0f82e498
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-03 20:11:08 -0800

    update

--- bin/py/csv_slice.py
+++ bin/py/csv_slice.py
@@ -1,11 +1,27 @@
+# read csv data (use this one for small, possibly irregular files)
 import os
 import sys
 import csv
 args = sys.argv
 
+if len(args) < 2:
+    print("csv_slice.py [input csv file] [variable to slice] .. [variable to slice]"); sys.exit(1)
+
+lines = []
 csv_file = open(args[1]) 
-
 reader = csv.reader(csv_file, delimiter=',')
-for row in reader:
-    print(row)
 
+
+count = {}
+for row in reader:
+    N = len(row)
+    if N not in count:
+        count[N] = 0
+    count[N] += 1
+    lines.append(row)
+print(count)
+
+hdr = lines[0]
+lines = lines[1:]
+
+print("to_slice", args[1:])
