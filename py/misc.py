6ab1001c68f2e142d7b9963780b0ff326a01729a
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-13 21:00:25 -0800

    update

--- py/misc.py
+++ py/misc.py
@@ -20,9 +20,45 @@ def run(cmd):
     return a
 
 
+def exists(f):
+    return os.path.exists(f)
+
+def assert_exists(f):
+    if not exists(f):
+        err("could not find req'd file: " + str(f))
+
+def load_fields(args): # load records and index by studyid
+    print("load_fields " + str(args))
+    dat, fn, load_fields = {}, args[0], args[1:]
+    assert_exists(fn)
+    f = open(fn)
+    fields = f.readline().strip().split(",") # read the header
+    field = {fields[i] : i for i in range(0, len(fields))}
+    print("fields", fields)
+    if 'studyid' not in fields: err("req'd field: studyid")
+    # make sure other required fields are present
+    for lf in load_fields:
+        if lf not in fields: err("req'd field: " + str(lf))
+    ci = 0
+    while True:
+        line = f.readline()
+        if not line: break
+        words = line.strip().split(",")
+        i, loaded = words[field['studyid']], []
+        if i not in dat: dat[i] = []
+        for lf in load_fields:
+            loaded.append(words[field[lf]])
+        dat[i].append(loaded)
+        ci += 1
+    print("loaded ", ci, " records")
+    return dat, load_fields
+
+
+
+
+
 # stopwatch / progress meter
 tick_last, tick_first = None, None
-
 
 def tick(index=None, total=None, message=None):
     global tick_last, tick_first
