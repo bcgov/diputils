a5637341b1955e9f440af3de5d45294db8b6d8c6
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-13 22:44:52 -0800

    update

---
+++ py/csv_info.py
@@ -0,0 +1,31 @@
+# guess data types of cols of csv. Ignore data with no info (one outcome only)
+import os
+import sys
+from misc import load_fields
+args = sys.argv
+info = len(args) > 3
+dat = load_fields("../test/merge.csv" if len(args) < 2 else args[1])
+
+types = {}
+for k in dat.keys():
+    d, is_float = dat[k], True # extract col
+    for i in range(0, len(d)):
+        try:
+            f = float(d[i])
+        except Exception:
+            is_float = False
+    
+    t = "float" if is_float else "str"
+    lsd = len(set(d))
+    if lsd > 1:
+        if info:
+            print(t, k, lsd if lsd > 12 else set(d))
+        if not t in types:
+            types[t] = []
+        types[t].append(k)
+
+if len(args) < 3:
+    print(types)
+else:
+    if args[2] in ['str', 'float']:
+        print(','.join(types[args[2]]))
