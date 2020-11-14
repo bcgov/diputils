340a373461943a9ec5eb7b257ac247d6d4475d54
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-13 21:46:37 -0800

    update

--- py/csv_slice.py
+++ py/csv_slice.py
@@ -4,6 +4,7 @@ import os
 import sys
 import csv
 args = sys.argv
+from misc import err
 
 if len(args) < 2:
     print("csv_slice.py [input csv file] [variable to slice] .. [variable to slice]"); sys.exit(1)
@@ -25,4 +26,16 @@ print(count)
 hdr = lines[0]
 lines = lines[1:]
 
-print("to_slice", args[1:])
+to_slice = args[1:]
+for field in to_slice:
+    if field not in hdr:
+        err("field not found in header: " + field)
+
+slice_i = [hdr.index(field) for field in to_slice]
+
+f = open(args[1] + "_slice.csv", "wb")
+f.write((','.join([hdr[i] for i in slice_i])).encode())
+for i in range(len(lines)):
+    f.write('\n' + (','.join([lines[i][j] for f in slice_i])).encode())
+f.close()
+
