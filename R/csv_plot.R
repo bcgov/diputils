7e4a7f7d861434f54e08ecd144b00907d7064c47
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-11-13 20:37:31 -0800

    update

---
+++ R/csv_plot.R
@@ -0,0 +1,31 @@
+#
+args = commandArgs(trailingOnly=TRUE)
+if(length(args)==0){
+  stop("csv_plot.R [csv file name]");
+}
+d = read.csv(file=args[1], header= TRUE)
+fields <- colnames(d)
+png(paste(args[1], "_plot.png", sep=''))
+
+dt <- (log(d + exp(1)) - 1)
+# dt <- d
+l <- lm(dt) #l <- lm(dt)
+print(l)
+# plot(l): LOOK AT THIS
+
+r_sq <-summary(l)$r.squared
+print(r_sq)
+
+plot(dt[,1] ~ dt[,2], xlab=paste("f(",fields[2],")"), ylab=paste("f(", fields[1], ")"))
+titl <- paste("f(", fields[1], ") vs f(", fields[2], ") with f(*) =log((*) + e) -1)")
+
+print(l)
+print(l[0])
+print(l[1])
+abline(l, col="red")#l$coefficients[0], l$coefficients[1], col="red")
+
+legend("topright", bty="n", legend=paste("r^2 =", format(r_sq, digits=4)), col="red")
+title(main = titl)
+dev.off()
+
+write.csv(r_sq, paste(args[1], "_rsq.txt", sep=''))
---
+++ R/dd_unpack.R
@@ -0,0 +1,10 @@
+# unpack data dictionaries in .xls files, into csv files. usage: Rscript dd_unpack.R [name of Excel file to unpack]
+library(readxl)
+
+fn<-""
+args = commandArgs(trailingOnly=TRUE)
+
+if(length(args)>=1) fn<-args[1]
+if(length(args)==0) stop("Error: usage: Rscript dd_unpack.R [data dictionary file.xls/xlsx]")
+
+for(s in excel_sheets(fn)){ out_fn<-paste(paste(fn, s, sep="_"), ".csv", sep=""); write.csv(read_excel(fn, sheet=s, skip=1), out_fn); }
