52e6538dfc129b979008b0593230c84b2a3cccf5
ashlinrichardson <richardson.ashlin@gmail.com>
2021-03-23 02:28:06 -0700

    update

--- fw2csv/test.sh
+++ fw2csv/test.sh
@@ -1,5 +1,5 @@
-g++ cpp/dd_apply.cpp cpp/misc.cpp -o dd_apply.exe
-tar xvf pnet.tar.gz
-mv  ./moh_dip_pharmanet_dsp.rpt.csv tmp.csv
-./dd_apply.exe moh_dip_pharmanet_dsp.rpt.dd moh_dip_pharmanet_dsp.rpt.dat
-diff tmp.csv moh_dip_pharmanet_dsp.rpt.dat_dd_apply.csv
+g++ cpp/dd_apply.cpp cpp/misc.cpp -o dd_apply.exe # compile terminal utility
+tar xvf pnet.tar.gz # extract test data
+mv  ./moh_dip_pharmanet_dsp.rpt.csv tmp.csv # move already-converted (pre-computed) file to tmp.csv to check with our result
+./dd_apply.exe moh_dip_pharmanet_dsp.rpt.dd moh_dip_pharmanet_dsp.rpt.dat # apply terminal utility
+diff tmp.csv moh_dip_pharmanet_dsp.rpt.dat_dd_apply.csv # check to see if pre-computed result matches what we made
